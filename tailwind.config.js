50e3304fbeaf7744ef68c17f53aa06036fc677eb
Akpo1705 <akpo23@gmail.com>
2024-09-19 15:37:32 +0300

    test new branch and commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0091D5',
+        secondary: '#F2F4F8',
+      },
+    },
   },
   plugins: [],
 }
